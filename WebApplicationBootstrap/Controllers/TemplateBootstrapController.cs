843fc861c5450c578dc41b24505de9854e2a3fd5
mohamed@3030eae <mohamed3030eae@gmail.com>
2020-06-20 14:58:14 +0200

    finshed

---
+++ WebApplicationBootstrap/Controllers/TemplateBootstrapController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationBootstrap.Controllers
+{
+    public class TemplateBootstrapController : Controller
+    {
+        // GET: TemplateBootstrap
+   
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplicationBootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationBootstrap.MvcApplication" Language="C#" %>
